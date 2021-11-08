a7151d80c3c108f4a5970ae514259e8a760060c4
MD. Jayeen Bin Alam <jayeen6783@gmail.com>
2021-11-09 00:12:31 +0600

    jwt token setted at safe position

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
+const admin = require("firebase-admin");
 const { MongoClient } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 //MIDDLEWARE
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+//VERIFY TOKEN
+const verifyToken = async (req, res, next) => {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers?.authorization?.split(' ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+
+    next();
+}
+//
+
 //MONGO CONNECT
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urbpc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -46,12 +73,21 @@ const run = async () => {
         })
 
         //ADD ADMIN ROLE
-        app.put('/users/admin', async (req, res) => {
+        app.put('/users/admin', verifyToken, async (req, res) => {
             const adminEmail = req.body.adminEmail;
-            const query = { email: adminEmail };
-            const updatedoc = { $set: { role: 'admin' } };
-            const result = await usersCollection.updateOne(query, updatedoc);
-            res.json(result);
+            const decodedUserEmail = req.decodedEmail;
+            if (decodedUserEmail) {
+                const requesterAccount = await usersCollection.findOne({ email: decodedUserEmail });
+                if (requesterAccount.role === 'admin') {
+                    const query = { email: adminEmail };
+                    const updatedoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(query, updatedoc);
+                    res.json(result);
+                }
+                else {
+                    res.status(403).json({ message: 'You dont have access to make an admin' })
+                }
+            }
         })
 
         //FILTER IF CURRENT USER ADMIN OR NOT
